ecc14a51f2c160a8c820f271166c25cfeeda9dfc
Meirah Williamson <58747733+meirahw@users.noreply.github.com>
2025-04-25 12:57:37 -0600

    Update index.js

--- maps/nl/index.js
+++ maps/nl/index.js
@@ -1,1 +1,7 @@
+var map = L.map(‘map’).setView([51.505, -0.09], 13);
+
+L.tileLayer(‘https://tile.openstreetmap.org/{z}/{x}/{y}.png’, { attribution: ‘© OpenStreetMap contributors’ }).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map) .bindPopup(‘A pretty CSS popup.
+Easily customizable.’) .openPopup();
 
