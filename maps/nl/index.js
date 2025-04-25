41896845d4080a4ed8ee50e8a260b1f0c7f0d0e6
Meirah Williamson <58747733+meirahw@users.noreply.github.com>
2025-04-25 13:02:30 -0600

    Delete maps/nl/index.js

--- maps/nl/index.js
+++
@@ -1,9 +0,0 @@
-<div id="my-custom-map" style="width: 600px; height: 400px"></div>
-
-var map = L.map(‘my-custom-map’).setView([51.505, -0.09], 13);
-
-L.tileLayer(‘https://tile.openstreetmap.org/{z}/{x}/{y}.png’, { attribution: ‘© OpenStreetMap contributors’ }).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map) .bindPopup(‘A pretty CSS popup.
-Easily customizable.’) .openPopup();
-
